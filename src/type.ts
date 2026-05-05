6c01863e892d9fd9396d60f41747e0f2bced59e2
aseli <aseli4488@gmail.com>
2026-05-05 13:27:02 +0900

    Fix: operator misconfigured

--- src/type.ts
+++ src/type.ts
@@ -8,10 +8,10 @@ export const TemplateMap = {
 
 export const OperatorMap = {
   "": "",
-  ">=": "1",
-  ">": "2",
-  "<=": "3",
-  "<": "4",
+  ">": "1",
+  ">=": "2",
+  "<": "3",
+  "<=": "4",
   "==": "5",
 } as const;
 
